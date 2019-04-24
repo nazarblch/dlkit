1e5b591e69cdcf5730f0dae1be1d86de730f36e9
nazar <you@example.com>
2019-04-24 13:31:04 +0300

    dcgan

--- framework/nn/modules/gan/GANLoss.py
+++ framework/nn/modules/gan/GANLoss.py
@@ -19,10 +19,10 @@ class GANLoss(ABC):
         self.discriminator = discriminator
 
     @abstractmethod
-    def _discriminator_loss(self, x: Tensor, y: Tensor) -> Loss: pass
+    def _discriminator_loss(self, real: Tensor, fake: Tensor) -> Loss: pass
 
     @abstractmethod
-    def _generator_loss(self, x: Tensor) -> Loss: pass
+    def _generator_loss(self, fake: Tensor) -> Loss: pass
 
     def add_penalty(self, pen: DiscriminatorPenalty) -> None:
         self.__penalties.append(pen)
@@ -33,12 +33,12 @@ class GANLoss(ABC):
     def add_generator_loss(self, loss: Callable[[Tensor], Loss]) -> None:
         self.__gen_losses.append(loss)
 
-    def discriminator_loss(self, x: Tensor, y: Tensor) -> Loss:
-        Dx = self.discriminator.forward(x.detach())
-        Dy = self.discriminator.forward(y.detach())
+    def discriminator_loss(self, real: Tensor, fake: Tensor) -> Loss:
+        Dx = self.discriminator.forward(real.detach())
+        Dy = self.discriminator.forward(fake.detach())
 
-        alpha = torch.rand(x.size(0), *((1,) * (x.ndimension() - 1)), device=x.device)
-        hat_x = alpha * x.detach() + (1 - alpha) * y.detach()
+        alpha = torch.rand(real.size(0), *((1,) * (real.ndimension() - 1)), device=real.device)
+        hat_x = alpha * real.detach() + (1 - alpha) * fake.detach()
 
         loss_sum: Loss = self._discriminator_loss(Dx, Dy)
 
@@ -50,12 +50,48 @@ class GANLoss(ABC):
 
         return loss_sum
 
-    def generator_loss(self, Gz: Tensor) -> Loss:
+    def generator_loss(self, fake: Tensor) -> Loss:
 
-        loss_sum: Loss = self._generator_loss(Gz)
+        loss_sum: Loss = self._generator_loss(fake)
 
         for loss in self.__gen_losses:
-            loss_sum += loss(Gz)
+            loss_sum += loss(fake)
 
         return loss_sum
+
+    def __add__(self, other):
+        return GANLossObject(
+            self.discriminator,
+            lambda real, fake: self.discriminator_loss(real, fake) + other.discriminator_loss(real, fake),
+            lambda fake: self.generator_loss(fake) + other.generator_loss(fake)
+        )
+
+    def __mul__(self, weight: float):
+
+        return GANLossObject(
+            self.discriminator,
+            lambda real, fake: self.discriminator_loss(real, fake) * weight,
+            lambda fake: self.generator_loss(fake) * weight
+        )
+
+
+class GANLossObject(GANLoss):
+    def _discriminator_loss(self, real: Tensor, fake: Tensor) -> Loss:
+        pass
+
+    def _generator_loss(self, fake: Tensor) -> Loss:
+        pass
+
+    def __init__(self, discriminator: Discriminator,
+                 discriminator_loss: Callable[[Tensor, Tensor], Loss],
+                 generator_loss: Callable[[Tensor], Loss]):
+        super().__init__(discriminator)
+        self.d_loss = discriminator_loss
+        self.g_loss = generator_loss
+
+    def discriminator_loss(self, real: Tensor, fake: Tensor) -> Loss:
+        return self.d_loss(real, fake)
+
+    def generator_loss(self, fake: Tensor) -> Loss:
+        return self.g_loss(fake)
 
